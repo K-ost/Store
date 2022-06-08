2c84308a2099011e547fec1fefedc0ff376203d7
Konstantin Golubkov <konstantingolubkov@MacBook-Air-Konstantin.local>
2022-06-08 09:11:59 +0300

    home page test git

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import LatestComments from '../components/LatestComments/LatestComments'
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Home</h1>
+      <h1>Home page</h1>
 
       <LatestComments />
     </Container>
